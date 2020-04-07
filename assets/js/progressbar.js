ec24aa3dd97559211cff758bc4acd1f9128f08e3
fadjarrafi <dark.fir21@gmail.com>
2020-04-07 13:42:59 +0700

    editing progress bar animation

--- assets/js/progressbar.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    var element = $(".bar-progress");
-    $(window).scroll(function () {
-      if($(window).scrollTop() > 0) {
-        element.addClass("html");
-      }
-      
-    });
-  });
